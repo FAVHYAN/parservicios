54483fccc12e6dfd0c9f7aa5e1fcb05f7ce9daca
andres.torres <andres.torres@parservicios.com>
2018-02-07 12:12:46 -0500

    inicio de primer estructura

---
+++ src/header.php
@@ -0,0 +1,32 @@
+
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" ></script>
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="js/bootstrap.min.js" ></script>
+
+</head>
+  		<div class="container">
+			<div class="row">
+				<header>
+					<nav class="navbar navbar-default">
+					  <div class="container-fluid">
+					    <div class="navbar-header">
+					      <a class="navbar-brand" href="#">AUTOMoviles</a>
+					    </div>
+					    <ul class="nav navbar-nav" style="float:right">
+					      <li><a href="<?php URL_PATH ?>/inxait/public/home/index">Home</a></li>
+						  <li><a href="<?php URL_PATH ?>/inxait/public/home/concurso">Concurso</a></li>  
+						  <li><a href="<?php URL_PATH ?>/inxait/public/home/registrar">Registrar</a></li>
+					    </ul>
+					  </div>
+					</nav>
+				</header>
+			</div>
+		</div>
+	<body>
+  		<div class="container">
